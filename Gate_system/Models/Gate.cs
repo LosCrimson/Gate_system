e4e0d0043df26c2ffa78e848f03c1b9940b7fa94
LosCrimson <lukas.suslavicius@gmail.com>
2022-12-13 22:29:38 +0200

    Setup some objects and alll enums

---
+++ Gate_system/Models/Gate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gate_system.Models
+{
+    internal class Gates
+    {
+    }
+}
